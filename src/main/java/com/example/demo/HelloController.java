53749f8601349322ebbe489a2892e3814b0912b2
praveen <praveen@invenio-solutions.come>
2020-04-13 13:11:14 +0530

    New commit1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -18,7 +18,7 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String getName(){
 		save();
-		return "Welcome every to jenkinsss";
+		return "Welcome every  to jenkinsss";
 		
 	}
 	
